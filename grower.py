2f2807912ebd93eb60842e21f053a9c773cd0e97
Jeff Trempe <trempe.jeff@gmail.com>
2020-05-11 23:23:56 -0400

    New save

--- grower.py
+++ grower.py
@@ -1,26 +1,28 @@
-import RPi.GPIO as GPIO
-import time
+# import RPi.GPIO as GPIO
+# import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
 
 class Gpio:
   'Common base class for all GPIOS'
 
-  def __init__(self, position):
-    GPIO.setup(position, GPIO.OUT)
+  def __init__(self, position, name):
+    # GPIO.setup(position, GPIO.OUT)
+    self.name = name
     self.position = position
 
   def getPosition(self):
-    print("Position: ",self.position)
+    print("Position for", self.name,": ",self.position)
 
   def turnOn(self):
-    GPIO.output(self.position,GPIO.LOW)
+    # GPIO.output(self.position,GPIO.LOW)
+    print("Turned on the", self.name)
 
   def turnOff(self):
-    GPIO.output(self.position,GPIO.HIGH)
+    # GPIO.output(self.position,GPIO.HIGH)
+    print("Turned off the", self.name)
 
+lights = Gpio(16,'lights')
 
-lights = Gpio(16)
-
-lights.getPosition()
+lights.turnOn()
