98f633de41f4ab2cedc7491f127c3ca8860ec23e
Jeff Trempe <trempe.jeff@gmail.com>
2020-05-12 08:31:03 -0400

    Seperated the files

---
+++ components/Gpio.py
@@ -0,0 +1,21 @@
+# import RPi.GPIO as GPIO
+# import time
+
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+
+class Gpio:
+  'Common base class for all GPIOS'
+
+  def __init__(self, position, name):
+    # GPIO.setup(position, GPIO.OUT)
+    self.name = name
+    self.position = position
+
+  def turnOn(self):
+    # GPIO.output(self.position,GPIO.LOW)
+    print("Turned on the", self.name)
+
+  def turnOff(self):
+    # GPIO.output(self.position,GPIO.HIGH)
+    print("Turned off the", self.name)
--- grower.py
+++ grower.py
@@ -4,25 +4,8 @@
 # GPIO.setwarnings(False)
 # GPIO.setmode(GPIO.BOARD)
 
-class Gpio:
-  'Common base class for all GPIOS'
-
-  def __init__(self, position, name):
-    # GPIO.setup(position, GPIO.OUT)
-    self.name = name
-    self.position = position
-
-  def getPosition(self):
-    print("Position for", self.name,": ",self.position)
-
-  def turnOn(self):
-    # GPIO.output(self.position,GPIO.LOW)
-    print("Turned on the", self.name)
-
-  def turnOff(self):
-    # GPIO.output(self.position,GPIO.HIGH)
-    print("Turned off the", self.name)
+from components.Gpio import Gpio
 
 lights = Gpio(16,'lights')
 
-lights.turnOn()
+lights.turnOff()
