e3c679bcf544d9e2f36c21551d4ef129a51e3869
Jeff Trempe <trempe.jeff@gmail.com>
2020-06-02 19:24:02 -0400

    Ready to pull on pi

--- components/Gpio.py
+++ components/Gpio.py
@@ -1,8 +1,8 @@
-# import RPi.GPIO as GPIO
-# import time
+import RPi.GPIO as GPIO
+import time
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
 class Gpio:
   'Common base class for all GPIOS'
--- grower.py
+++ grower.py
@@ -1,8 +1,8 @@
-# import RPi.GPIO as GPIO
-# import time
+import RPi.GPIO as GPIO
+import time
 
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
 from components.Gpio import Gpio
 
@@ -10,3 +10,5 @@ lights = Gpio(16,'lights')
 
 lights.on()
 lights.off()
+
+
